b1cabe60de6c2c697d5ea5ccaa27c683dabcef00
mathurishani <91481581+mathurishani@users.noreply.github.com>
2023-09-18 00:09:23 +0530

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  toshow = '0';
+  currVal = '';
+
+  writeInput(value:string){
+    this.currVal = this.currVal + value
+    this.toshow = this.currVal
+  }
+
+  equals() {
+    this.toshow = eval(this.currVal)
+    this.currVal = this.toshow
+  }
+
+  clear() {
+    this.currVal = ' '
+    this.toshow = '0'
+  }
+
+  backInput() {
+    this.currVal = this.currVal.slice(0,-1)
+    this.toshow = this.currVal 
+    if(this.toshow == ''){this.toshow = '0'}
+    
+  }
+
+  calcValue(solve:any) {
+    if(solve.charAt(0) == '0'){
+      solve = solve.slice(1,)
+    }
+    this.toshow = eval(solve)
+  }
 }
